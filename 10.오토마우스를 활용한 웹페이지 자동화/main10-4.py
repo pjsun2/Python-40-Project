0c1a6997aed576a5990ed137dff2beddbfe533ab
PJS <pjsun2@m365.dongyang.ac.kr>
2024-11-24 19:28:40 +0900

    추가

--- 10.오토마우스를 활용한 웹페이지 자동화/main10-4.py
+++ 10.오토마우스를 활용한 웹페이지 자동화/main10-4.py
@@ -13,17 +13,19 @@ end_y = 936
 
 for 지역날씨 in 날씨:
     pyautogui.moveTo(addr_x,addr_y)
+
     time.sleep(0.2)
     pyautogui.click()
     time.sleep(0.2)
     pyautogui.write("www.naver.com", interval=0.1)
     pyautogui.write(["enter"])
-    time.sleep(0.2)
+    time.sleep(1)
 
     pyperclip.copy(지역날씨)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(0.5)
+     
     pyautogui.write(["enter"])
     time.sleep(1)
-    저장경로 = '10.오토마우스를 활용한 웹페이지 자동화\\' + 지역날씨 + '.png'
+    저장경로 = 'D:\\Code\\Python-40-Project\\10.오토마우스를 활용한 웹페이지 자동화\\' + 지역날씨 + '.png'
     pyautogui.screenshot(저장경로, region=(start_x,start_y,end_x-start_x, end_y-start_y))
