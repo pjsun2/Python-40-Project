283c6d4fb595d1d96136187557bd359bf6dd17ad
PJS <pjsun2@m365.dongyang.ac.kr>
2025-03-14 17:34:41 +0900

    소스 추가

---
+++ 12.exel/12.exel-2.py
@@ -0,0 +1,76 @@
+import docx
+from docx.oxml.ns import qn
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import docx.shared
+
+doc = docx.Document(r'D:\Code\Python-40-Project\12.exel\수료증양식.docx')
+
+style = doc.styles['Normal']
+style.font.name = '나눔고딕'
+style._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+style.font.size = docx.shared.Pt(12)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run('제 2020-0001 호\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run('수 료 증')
+run.font.name = '나눔고딕'
+run.bold = True
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(40)
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run(' 성 명 : 장다인\n')
+run.font.name = '나눔고딕'
+run.element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+run = para.add_run(' 생 년 월 일 : 2017.12.12\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+run = para.add_run(' 교 육 과 정: 파이썬과 40개의 작품들\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+run = para.add_run(' 교 육 날 짜: 2020.08.05~2021.09.09\n')
+run.font.name = '나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n')
+run = para.add_run(' 위 사람은 파이썬과 40개의 작품들 교육과정을\n')
+run.font.name='나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+run = para.add_run(' 이수하였으므로 이 증서를 수여 합니다.\n')
+run.font.name='나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n\n')
+run = para.add_run('2021.09.19')
+run.font.name='나눔고딕'
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+para = doc.add_paragraph()
+run = para.add_run('\n\n\n')
+run = para.add_run('파이썬교육기관장')
+run.font.name='나눔고딕'
+run.bold = True
+run._element.rPr.rFonts.set(qn('w:eastAsia'), '나눔고딕')
+run.font.size = docx.shared.Pt(20)
+para.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+doc.save(r'D:\Code\Python-40-Project\12.exel\수료증결과.docx')
